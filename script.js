21a471d599c05899d6342f7e4f74d3b801fa255b
rogue-captain <mr.suleyman.erkan@gmail.com>
2024-08-12 18:11:24 -0400

    Remove .DS_Store file

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
     window.onload = function() {
+
         window.scrollTo(0, 0);
+    
+        var header = document.getElementById("sullys-welcome");
+
+        header.classList.add("slideIn");
     } ;
